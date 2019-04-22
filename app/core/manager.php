5f3da0433c7a15f3ab80a39a6ee6121ad4f3e2e0
HardCoreWare <bernardocodu@gmail.com>
2019-04-22 11:24:41 -0500

    activando modelos tipo modulo

--- app/core/manager.php
+++ app/core/manager.php
@@ -5,5 +5,7 @@ require_once '../app/model/interface/ConnectionI.php';
 require_once '../app/model/interface/TableI.php';
 require_once '../app/model/primitive/Connection.php';
 require_once '../app/model/primitive/Table.php';
+require_once '../app/model/module/Cecos.php';
+
 
 ?>
