8f60ee4f1d9fb76fc7a14dea87011e389c4ea514
HardCoreWare <bernardocodu@gmail.com>
2019-04-22 16:00:19 -0500

    ok

--- app/model/module/Bseg.php
+++ app/model/module/Bseg.php
@@ -12,7 +12,11 @@ class Bseg extends Table{
 
     public function index(){
 
-
+        $sql ="SELECT BUKRS AS sociedadSap, CAST(DMBTR AS FLOAT64) AS importe, KOSTL AS ceco, BKTXT AS descripcion, HKONT AS cuenta,".
+        " CONCAT(SUBSTR(BUDAT,1,4),'/',SUBSTR(BUDAT,5,2),'/',SUBSTR(BUDAT,7,2)) AS fecha".
+        " FROM `informe-211921.CONTABLES.Bsegaio`".
+        " WHERE KOSTL IN (SELECT numero FROM `informe-211921.CONTABLES.Cecos`)".
+        " ORDER BY CAST(BUDAT AS INT64)";
 
     }
 
