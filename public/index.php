011858d5093f118f403aad2fc663683c56d0ce35
HardCoreWare <bernardocodu@gmail.com>
2019-04-22 11:11:24 -0500

    activando dependencia nueva

--- public/index.php
+++ public/index.php
@@ -1,10 +1,8 @@
 <?php
 
-//
 require_once '../app/vendor/autoload.php';
 require_once '../app/core/manager.php';
 
-//
 require_once '../app/core/controller.php';
 require_once '../app/core/router.php';
 
