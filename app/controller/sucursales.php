10971c4531d7e7ba2d7b0f05695c11ec1df583c8
HardCoreWare <bernardocodu@gmail.com>
2019-04-22 11:56:43 -0500

    namespaces

--- app/controller/sucursales.php
+++ app/controller/sucursales.php
@@ -2,6 +2,14 @@
 
 class sucursales{
 
+    public function __construct(){
+
+        header("Access-Control-Allow-Origin: *");
+        header('Content-Type: application/json');
+
+
+    }
+
     public function index(){
 
         $cecosModel = new Pit\Multiva\Contables\Cecos(BigQuery::getInstance('informe-211921'));
