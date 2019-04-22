32adcfc130e8dcfa86f97c8bac964622caec26c0
HardCoreWare <bernardocodu@gmail.com>
2019-04-22 17:32:50 -0500

    ajustando conceptos y agregando cuentas en bq

---
+++ app/model/module/Account.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Pit\Multiva\Contables;
+
+class Account extends Table{
+
+    public function __construct($bigQuery){
+
+        $this->attach($bigQuery);
+
+    }
+
+    public function index(){
+
+        $sql = "SELECT id,numero AS ceco,area,sucursal FROM `informe-211921.CONTABLES.Cecos` ORDER BY id;";
+        $cecos = $this->bigQuery->select($sql);
+        return $cecos;
+
+    }
+    
+}
+
+
+?>
