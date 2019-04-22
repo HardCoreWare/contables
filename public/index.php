add09b0c9a201910b0d1540d92cb62baa7f60d95
HardCoreWare <bernardocodu@gmail.com>
2019-04-22 10:13:47 -0500

    agregamos enrutador y le agregamos controlador de prueba unitaria

--- public/index.php
+++ public/index.php
@@ -1,7 +1,11 @@
 <?php
 
-require_once '../app/vendor/autoload.php';
+//
 require_once '../app/core/manager.php';
 require_once '../app/core/app.php';
+
+//
+require_once '../app/core/router.php';
+
 
 ?>
