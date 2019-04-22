e922dd52111260364aaa2afc8cd5b5bf71a3ddb6
HardCoreWare <bernardocodu@gmail.com>
2019-04-22 11:40:23 -0500

    namespaces

--- app/controller/sucursales.php
+++ app/controller/sucursales.php
@@ -5,6 +5,8 @@ class sucursales{
     public function index(){
 
         $cecosModel = new Pit\Multiva\Contables\Cecos(BigQuery::getInstance('informe-211921'));
+
+        $cecos = $cecosModel->index();
         
     }
     
