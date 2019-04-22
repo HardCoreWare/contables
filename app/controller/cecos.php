a742a066c4fc0442798f940deefd1b7915adadd6
HardCoreWare <bernardocodu@gmail.com>
2019-04-22 18:17:22 -0500

    corrigiendo controlador

--- app/controller/cecos.php
+++ app/controller/cecos.php
@@ -1,6 +1,6 @@
 <?php
 
-class sucursales{
+class cecos{
 
     public function __construct(){
 
@@ -11,7 +11,7 @@ class sucursales{
 
     public function index(){
 
-        $cecosModel = new Pit\Multiva\Contables\Cecos(BigQuery::getInstance('informe-211921'));
+        $cecosModel = new Pit\Multiva\Contables\Ceco(BigQuery::getInstance('informe-211921'));
         $cecos = $cecosModel->index();
         echo(json_encode($cecos));
         
--- app/core/manager.php
+++ app/core/manager.php
@@ -5,7 +5,7 @@ require_once '../app/model/interface/ConnectionI.php';
 require_once '../app/model/interface/TableI.php';
 require_once '../app/model/primitive/Connection.php';
 require_once '../app/model/primitive/Table.php';
-require_once '../app/model/module/Cecos.php';
+require_once '../app/model/module/Ceco.php';
 require_once '../app/model/module/Account.php';
 require_once '../app/model/module/Concept.php';
 require_once '../app/model/module/Bseg.php';
