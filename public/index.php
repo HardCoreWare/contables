7caa202db1aaa2cc121c631d314a9f837d024e1b
HardCoreWare <bernardocodu@gmail.com>
2019-04-22 10:54:36 -0500

    activando dependencia de composer bq

--- public/index.php
+++ public/index.php
@@ -1,15 +1,13 @@
 <?php
 
 //
+require_once '../app/vendor/autoload.php';
 require_once '../app/core/manager.php';
-
-//
 
 //
 require_once '../app/core/controller.php';
 require_once '../app/core/router.php';
 
 $router= new Router();
-
 
 ?>
