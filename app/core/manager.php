0c36e38f5634f9a856ed10bf66d4b4995320c1c8
HardCoreWare <bernardocodu@gmail.com>
2019-04-22 15:13:04 -0500

    ok

--- app/core/manager.php
+++ app/core/manager.php
@@ -6,5 +6,7 @@ require_once '../app/model/interface/TableI.php';
 require_once '../app/model/primitive/Connection.php';
 require_once '../app/model/primitive/Table.php';
 require_once '../app/model/module/Cecos.php';
+require_once '../app/model/module/Account.php';
+
 
 ?>
