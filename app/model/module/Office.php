6ad263308c82427be6ddd233fc0e79856558809e
HardCoreWare <bernardocodu@gmail.com>
2019-04-23 11:01:06 -0500

    listo

---
+++ app/model/module/Office.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Pit\Multiva\Contables;
+
+class Concept extends Table{
+
+    public function __construct($bigQuery){
+
+        $this->attach($bigQuery);
+
+    }
+
+    public function index(){
+
+        $sql = "SELECT id AS idSucursal, area, sucursal ".
+        " FROM `informe-211921.CONTABLES.Conceptos` ORDER BY id";
+
+
+        $cuentas = $this->bigQuery->select($sql);
+        return $cuentas;
+
+    }
+
+}
+
+?>
