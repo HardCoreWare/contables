451ec99252d750c26a408cc4a48ecbaac9640919
HardCoreWare <bernardocodu@gmail.com>
2019-04-23 21:30:16 -0500

    preparando esquema corregido

--- app/model/module/Bseg.php
+++ app/model/module/Bseg.php
@@ -15,7 +15,7 @@ class Bseg extends Table{
         $sql ="SELECT BUKRS AS sociedadSap, CAST(DMBTR AS FLOAT64) AS importe, KOSTL AS ceco, BKTXT AS descripcion, HKONT AS cuenta,".
         " CONCAT(SUBSTR(BUDAT,1,4),'/',SUBSTR(BUDAT,5,2),'/',SUBSTR(BUDAT,7,2)) AS fecha".
         " FROM `informe-211921.CONTABLES.Bsegaio`".
-        " WHERE KOSTL IN (SELECT numero FROM `informe-211921.CONTABLES.Cecos`)".
+        " WHERE KOSTL IN (SELECT numero FROM `informe-211921.CONTABLES.Ceco`)".
         " ORDER BY CAST(BUDAT AS INT64)";
         $bseg=$this->bigQuery->select($sql);
         return $bseg;
