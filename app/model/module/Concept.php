04508b7f2fa48cfd7900200b819cb6f611e9bfda
HardCoreWare <bernardocodu@gmail.com>
2019-04-23 21:20:42 -0500

    ok

--- app/model/module/Concept.php
+++ app/model/module/Concept.php
@@ -13,8 +13,8 @@ class Concept extends Table{
     public function index(){
 
         $sql = "SELECT id AS idConcepto, superConcepto, concepto, prorrateable ".
-        " FROM `informe-211921.CONTABLES.Conceptos` ORDER BY id";
-
+        " FROM `informe-211921.CONTABLES.Conceptos`".
+        " ORDER BY id";
 
         $cuentas = $this->bigQuery->select($sql);
         return $cuentas;
