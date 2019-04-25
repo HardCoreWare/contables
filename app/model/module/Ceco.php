dd9b3f1a53d378407863b3625cdd9daed300ca9f
HardCoreWare <bernardocodu@gmail.com>
2019-04-25 15:40:56 -0500

    ajustamos nuevo esquema de tabla aumentado

--- app/model/module/Ceco.php
+++ app/model/module/Ceco.php
@@ -12,7 +12,7 @@ class Ceco extends Table{
 
     public function index(){
 
-        $sql = "SELECT idSucursal, ceco ".
+        $sql = "SELECT idSucursal AS idDepartamento, ceco ".
         " FROM `informe-211921.CONTABLES.Ceco` ORDER BY idSucursal;";
         $cecos = $this->bigQuery->select($sql);
         return $cecos;
@@ -20,6 +20,5 @@ class Ceco extends Table{
     }
     
 }
-
 
 ?>
