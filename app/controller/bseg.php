05dc6ac923aec0dbf478003ed1d6775d67188457
HardCoreWare <bernardocodu@gmail.com>
2019-04-26 16:50:43 -0500

    ruta partida

--- app/controller/bseg.php
+++ app/controller/bseg.php
@@ -17,13 +17,6 @@ class Bseg extends Controller{
 
     }
 
-    public function mes($month){
-
-        $accountModel = new Pit\Multiva\Contables\Bseg(BigQuery::getInstance('informe-211921'));
-        $account = $accountModel->month($month);
-        echo(json_encode($account));
-
-    }
 
 }
 
