931a5b76573d7000ed5e065b4c3ca16668f72847
HardCoreWare <bernardocodu@gmail.com>
2019-04-26 01:35:31 -0500

    aumentando datos y dinamizando

--- app/model/module/Bseg.php
+++ app/model/module/Bseg.php
@@ -24,6 +24,19 @@ class Bseg extends Table{
 
     }
 
+    public function month($month){
+
+        $sql =
+        " SELECT BELNR AS numeroDocumento, BLART AS tipoDocumento, KOSTL AS referencia, PSWSL AS moneda, BUKRS AS sociedadSap, ROUND(CAST(DMBTR AS FLOAT64),2) AS importe, KOSTL AS ceco, BKTXT AS descripcion, HKONT AS cuenta,".
+        " CONCAT(SUBSTR(BUDAT,1,4),'-',SUBSTR(BUDAT,5,2),'-',SUBSTR(BUDAT,7,2)) AS fecha".
+        " FROM `informe-211921.CONTABLES.Bsegaio`".
+        " WHERE KOSTL IN (SELECT DISTINCT(ceco) FROM `informe-211921.CONTABLES.Ceco`)".
+        " AND CAST(SUBSTR(BUDAT,5,2) AS INT64) = ".$month.".
+        " AND HKONT IN(SELECT DISTINCT(cuenta) FROM `informe-211921.CONTABLES.Cuentas`)".
+        " ORDER BY CAST(BUDAT AS INT64)";
+
+    }
+
 }
 
 ?>
