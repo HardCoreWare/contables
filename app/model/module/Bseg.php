62580775c041d80f71029b213e92d5c44daf1a22
HardCoreWare <bernardocodu@gmail.com>
2019-04-26 17:27:40 -0500

    ruta partida

--- app/model/module/Bseg.php
+++ app/model/module/Bseg.php
@@ -14,7 +14,7 @@ class Bseg extends Table{
 
         $sql =
         " SELECT BELNR AS numeroDocumento, BLART AS tipoDocumento, KOSTL AS referencia, PSWSL AS moneda, BUKRS AS sociedadSap, ROUND(CAST(DMBTR AS FLOAT64),2) AS importe, KOSTL AS ceco, BKTXT AS descripcion, HKONT AS cuenta,".
-        " CONCAT(SUBSTR(BUDAT,1,4),'-',SUBSTR(BUDAT,5,2),'-',SUBSTR(BUDAT,7,2)) AS fecha, SUBSTR(BUDAT,5,2) AS mes".
+        " CONCAT(SUBSTR(BUDAT,1,4),'-',SUBSTR(BUDAT,5,2),'-',SUBSTR(BUDAT,7,2)) AS fecha, CAST(SUBSTR(BUDAT,5,2) AS INT64) AS mes".
         " FROM `informe-211921.CONTABLES.Bsegaio`".
         " WHERE KOSTL IN (SELECT DISTINCT(ceco) FROM `informe-211921.CONTABLES.Ceco`)".
         " AND HKONT IN (SELECT DISTINCT(cuenta) FROM `informe-211921.CONTABLES.Cuentas`)".
